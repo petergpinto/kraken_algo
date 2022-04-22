e5b8e8bf2b69205ff4dc9a12f5ff5246e5cdc24f
Peter Pinto <peter@peterpinto.dev>
2022-04-22 19:37:58 -0400

    Added python script to get latest ohlc data by the minute for each trading pair in the database and added a systemd file to continuously run the script

--- get-ohlc-data.py
+++ get-ohlc-data.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import json
 import os
 from pprint import pprint
+import time
 
 load_dotenv()
 
@@ -18,8 +19,7 @@ cursor = cnx.cursor()
 
 # configure api
 k = krakenex.API()
-k.load_key('combined.key')
-
+k.load_key(os.environ['KRAKEN_API_KEY'])
 
 #Possible intervals to request
 oneMinute=1
@@ -31,3 +31,39 @@ fourHours=240
 oneDay=1440
 oneWeek=10080 
 fifteenDays=21600
+
+
+query="SELECT * FROM tradable_pairs"
+
+cursor.execute(query)
+
+#Response format
+#"time": 1625405820000,
+#"open": "35399.75000000000",
+#"high": "35407.25000000000",
+#"low": "35388.50000000000",
+#"close": "35390.25000000000",
+# ?? unknown value ??
+# volume?
+# trades?
+
+insert_ohlc="INSERT IGNORE INTO ohlc_data (pairId, timestamp, open, high, low, close, volume, trades) VALUES (%s, FROM_UNIXTIME(%s), %s, %s, %s, %s, %s, %s)"
+latest_timestamp="SELECT timestamp from ohlc_data WHERE pairId=%s order by timestamp desc limit 1"
+
+tradable = cursor.fetchall()
+
+while True:
+	print("Starting OHLC data collection...")
+	for id, pair, base, altname, orderMin in tradable:
+		cursor.execute(latest_timestamp, (id, ) )
+		timestamp = cursor.fetchall()[0][0].timestamp()
+		req_data = {'docalcs': 'true', 'pair':pair, 'interval':oneMinute, 'since' : int(timestamp) }
+		OHLC_data = k.query_public('OHLC', req_data)
+		for timestamp,open,high,low,close,unknown,volume,trades in OHLC_data['result'][pair]:
+			cursor.execute(insert_ohlc, (id, timestamp, open, high, low, close, volume, trades))
+		cnx.commit()
+	print("Fetched all, sleeping for 5 minutes...")
+	time.sleep(300)
+
+cnx.commit()
+cnx.close()
