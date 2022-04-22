52baf02af0a71ad685f0ff25b15e4de89479f985
Peter Pinto <peter@peterpinto.dev>
2022-04-22 14:43:15 -0400

    Updated python script to insert tradable pairs into database

--- get-all-tradable-pairs.py
+++ get-all-tradable-pairs.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 import krakenex
+import mysql.connector
+from dotenv import load_dotenv
+import json
+import os
 from pprint import pprint
+
+load_dotenv()
+
+cnx = mysql.connector.connect(
+	user=os.environ['MYSQL_DB_USER'],
+	password=os.environ['MYSQL_DB_PASS'],
+	host=os.environ['MYSQL_DB_HOST'],
+	database=os.environ['MYSQL_DB_NAME']
+)
+cursor = cnx.cursor()
 
 # configure api
 k = krakenex.API()
@@ -18,26 +32,14 @@ latency = end['result']['unixtime'] - start['result']['unixtime']
 # parse result
 #b, c = 0, 0
 
-pprint(open_positions)
+add_pair = "INSERT IGNORE INTO tradable_pairs (pair, base, altname, orderMin) VALUES (%s, %s, %s, %s)"
 
-'''
-for order in open_positions['result']:
-    coin = order["pair"]
-    if coin == 'XETHZUSD':
-        b += (float(order['vol']))
-    elif coin == 'XXBTZUSD':
-        c += (float(order['vol']))
 
-n_errors = len(open_positions['error'])
-total = len(open_positions['result'])
+for pair, data in open_positions['result'].items():
+	cursor.execute(add_pair, (pair, data['base'], data['altname'], data['ordermin']))
 
-msg = """
-error counts: {n_errors}
-latency: {latency}
-open orders
-    eth: {b}
-    btc: {c}
-    total: {total}
-"""
-print(msg.format(n_errors=n_errors, total=total, b=b, c=c, latency=latency))
-'''
+
+cnx.commit()
+
+cursor.close()
+cnx.close()
